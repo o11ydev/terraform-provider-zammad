295d527e0647ba798cb7be83518a84fe35093aaf
Julien Pivotto <roidelapluie@o11y.eu>
2022-07-01 15:25:46 +0200

    Fix tests
    Signed-off-by: Julien Pivotto <roidelapluie@o11y.eu>

--- zammad/provider.go
+++ zammad/provider.go
@@ -23,7 +23,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/logging"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
 
 	"github.com/o11ydev/terraform-provider-zammad/internal/client"
 )
